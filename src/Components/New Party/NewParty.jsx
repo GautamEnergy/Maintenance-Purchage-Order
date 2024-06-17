406d9c7c3103b3774938104e17123e377c4911bd
KULBHUSHANSINGH-arch <gautamenergy526@gmail.com>
2024-06-17 18:35:22 +0530

    updated NewParty form

--- src/Components/New Party/NewParty.jsx
+++ src/Components/New Party/NewParty.jsx
@@ -6,70 +6,76 @@ const NewParty = () => {
     const [GST, SetGST] = useState('');
     const [PAN, SetPAN] = useState('')
 
-    /**For Gst  to get the data */
+    /**         ---For Gst  to get the data         */
     const onGSTHandler = (e) => {
         SetGST(e.target.value)
     }
     /** Gst to get the Pan Number & Call the Api for it  */
-    const GetPANNumberByGST = async () => {
-        const { data } = await axios.get(`http://localhost:9001/verify-gst/${GST}`)
-        console.log(data)
-    }
+    // const GetPANNumberByGST = async () => {
+    //     const { data } = await axios.get(`http://localhost:9001/verify-gst/${GST}`)
+    //     console.log(data)
+    // }
 
 
-    const handleSubmit = (e) => {
+    const handleSave = (e) => {
         e.preventDefault();
         console.log('Form data submitted:');
     };
 
     return (
-        <form onSubmit={handleSubmit} className="container mt-5">
+        <form
+            // onSubmit={handleSave} 
+            className="first-mainCard mt-5">
             <h4 className="text-center" >Add New Party</h4>
             <div className="form-group">
                 <label>GST No.</label>
-                <input type="text" className="form-control" name="gstNo" value={GST} onChange={onGSTHandler} />
+                <input type="text" className="form-text" name="gstNo" value={GST} onChange={onGSTHandler} />
             </div>
 
 
             <div className="form-group">
                 <label>Party Name</label>
-                <input type="text" className="form-control" name="partyName"
+                <input type="text" className="form-text" name="partyName"
                 />
             </div>
 
             <div className="form-group">
                 <label>Address</label>
-                <input type="text" className="form-control" name="address" />
+                <input type="text" className="form-text" name="address" />
             </div>
 
             <div className="form-group">
                 <label>Country</label>
-                <input type="text" className="form-control" name="country" />
+                <input type="text" className="form-text" name="country" />
             </div>
 
             <div className="form-group">
                 <label>State</label>
-                <input type="text" className="form-control" name="state" />
+                <input type="text" className="form-text" name="state" />
             </div>
 
             <div className="form-group">
                 <label>Pin</label>
-                <input type="text" className="form-control" name="pin" />
+                <input type="text" className="form-text" name="pin" />
             </div>
 
             <div className="form-group">
                 <label>Mobile No.</label>
-                <input type="text" className="form-control" name="mobileNo" />
+                <input type="text" className="form-text" name="mobileNo" />
             </div>
 
             <div className="form-group">
                 <label>Email</label>
-                <input type="email" className="form-control" name="email" />
+                <input type="email" className="form-text" name="email" />
             </div>
 
             <div className="form-group">
                 <label>PAN No.</label>
-                <input type="text" className="form-control" name="panNo" value={PAN} onClick={GetPANNumberByGST} />
+                <input type="text"
+                    className="form-text" name="panNo"
+                // value={PAN}
+                //  onClick={GetPANNumberByGST}
+                />
             </div>
 
 
