d13b8bc38508c1726dc0d6b988a65282dbc17376
KULBHUSHANSINGH-arch <gautamenergy526@gmail.com>
2024-06-28 14:08:58 +0530

    add Spare Part master By Kulbhushan Singh

--- src/Components/AddSpare/AddSpare.jsx
+++ src/Components/AddSpare/AddSpare.jsx
@@ -1,42 +1,23 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import { FaTimes } from 'react-icons/fa';
+// import { FaTimes } from 'react-icons/fa';
 import 'react-toastify/dist/ReactToastify.css';
-import '../AddSpare/AddSpare.css';
+import './AddSpare.css';
 
 const AddSpare = () => {
     const [MasterSpare, setMasterSpare] = useState('');
-    
     const [SparePartName, setSparePartName] = useState('');
     const [SparePartModelNo, setSparePartModelNo] = useState('');
     const [Brand, setBrand] = useState('');
     const [Specification, setSpecification] = useState('');
     const [MacineName, setMacineName] = useState('');
     const [MachineModelNo, setMachineModelNo] = useState('');
-    
     const [Status, setStatus] = useState('Active');
-
     const [error, setError] = useState('');
-
-
 
     const notifySuccess = () => toast.success("New Spare Part Added Successfully!", { autoClose: 5000 });
     const notifyError = (message) => toast.error(message, { autoClose: 5000 });
 
-    // const handleGSTChange = (e) => {
-    //     const gstValue = e.target.value.toUpperCase();
-    //     setGSTNumber(gstValue);
-    //     if (validateGST(gstValue)) {
-    //         extractPANFromGST(gstValue);
-    //         setError('');
-    //     } else {
-    //         setPANNumber('');
-    //         setError('Invalid GST number');
-    //     }
-    // };
-
-   
-   
     const addNewSpare = async (SpareData) => {
         try {
             const response = await fetch('http://srv515471.hstgr.cloud:8080/Maintenance/AddSpare', {
@@ -49,7 +30,6 @@ const AddSpare = () => {
             if (response.ok) {
                 notifySuccess();
                 setMasterSpare('');
-                
                 setSparePartName('');
                 setSparePartModelNo('');
                 setBrand('');
@@ -57,7 +37,6 @@ const AddSpare = () => {
                 setMacineName('');
                 setMachineModelNo('');
                 setStatus('Active');
-
                 setError('');
             } else {
                 const errorData = await response.json();
@@ -70,18 +49,15 @@ const AddSpare = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (MasterSpare && GSTNumber && PANNumber && SparePartName && SparePartModelNo && Brand && Specification && MacineName && Status) {
+        if (MasterSpare && SparePartName && SparePartModelNo && Brand && Specification && MacineName && Status) {
             const SpareData = {
                 MasterSpare,
-                GSTNumber,
-                PANNumber,
                 Brand,
                 Specification,
                 MacineName,
                 SparePartModelNo,
                 SparePartName,
                 Status,
-
             };
             console.log('Form data submitted:', SpareData);
             addNewSpare(SpareData);
@@ -95,94 +71,86 @@ const AddSpare = () => {
     };
 
     return (
-        <>
-            <form onSubmit={handleSubmit} className="first-mainCard mt-5">
-                <div className="header">
-                    <h4 className="text-center">
-                        <b>Add New Spare</b></h4>  <FaTimes className="close-icon" onClick={handleClose} />
-                </div>
-                <div className="form-group">
-                    <label> Master Spare Name</label>
-                    <input type="text" className="form-text" name="MasterSpare"
+        <div className="Section1">
+            <div className="form-header">
+                <h2 className="form-title">Add New Spare</h2>
+                {/* <FaTimes className="close-icon" onClick={handleClose} /> */}
+            </div>
+            <form onSubmit={handleSubmit}>
+                <div className="system">
+                    <input
+                        type="text"
+                        name="MasterSpare"
                         value={MasterSpare}
                         onChange={(e) => setMasterSpare(e.target.value)}
-                        required />
+                        required
+                    />
+                    <label>Master Spare Name</label>
                 </div>
-                <div className="form-group">
-                    <label>Spare Name</label>
-                    <input type="text" className="form-text" name="SparePart"
+                <div className="system">
+                    <input
+                        type="text"
+                        name="SparePart"
                         value={SparePartName}
                         onChange={(e) => setSparePartName(e.target.value)}
-                        required />
+                        required
+                    />
+                    <label>Spare Name</label>
                 </div>
-                <div className="form-group">
-                    <label>Spare Part Model Name</label>
-                    <input type="text" className="form-text" name="SparePartmodel"
-                        value={SparePartModelNo}
-                        onChange={(e) => setSparePartModelNo(e.target.value)}
-                        required />
-                </div>
-                
-                
-                <div className="form-group">
-                    <label>SparePartModelNo </label>
+                <div className="system">
                     <input
-                        type="sparepartmodelno"
-                        className="form-text"
-                        name="sparepartmodelno"
+                        type="text"
+                        name="SparePartModelNo"
                         value={SparePartModelNo}
                         onChange={(e) => setSparePartModelNo(e.target.value)}
                         required
                     />
+                    <label>Spare Part Model Name</label>
                 </div>
-                <div className="form-group">
-                    <label>Brand</label>
+                <div className="system">
                     <input
                         type="text"
-                        className="form-text"
                         name="brand"
                         value={Brand}
                         onChange={(e) => setBrand(e.target.value)}
                         required
                     />
+                    <label>Brand</label>
                 </div>
-                <div className="form-group">
-                    <label>Specification</label>
+                <div className="system">
                     <input
                         type="text"
-                        className="form-text"
                         name="specification"
                         value={Specification}
                         onChange={(e) => setSpecification(e.target.value)}
                         required
                     />
+                    <label>Specification</label>
                 </div>
-                <div className="form-group">
-                    <label>MacineName</label>
+                <div className="system">
                     <input
                         type="text"
-                        className="form-text"
-                        name="macinename"
+                        name="macineName"
                         value={MacineName}
                         onChange={(e) => setMacineName(e.target.value)}
                         required
                     />
+                    <label>Machine Name</label>
                 </div>
-                <div className="form-group">
-                    <label>Machine Model Number</label>
+                <div className="system">
                     <input
                         type="text"
-                        className="form-text"
-                        name="macinemodel"
+                        name="machineModelNo"
                         value={MachineModelNo}
                         onChange={(e) => setMachineModelNo(e.target.value)}
                         required
                     />
+                    <label>Machine Model Number</label>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="submit-button">Submit</button>
             </form>
             <ToastContainer position="top-center" autoClose={2000} />
-        </>
+        </div>
     );
 };
 
