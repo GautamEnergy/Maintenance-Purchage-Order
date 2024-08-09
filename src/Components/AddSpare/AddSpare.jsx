40eca038c65fe1195c086ce37c68bc2f436e1a54
KULBHUSHANSINGH-arch <gautamenergy526@gmail.com>
2024-08-09 16:56:17 +0530

    update Add spare

--- src/Components/AddSpare/AddSpare.jsx
+++ src/Components/AddSpare/AddSpare.jsx
@@ -277,7 +277,7 @@ const AddSpare = () => {
 
         formData.append('DrawingImage', pdf);
         formData.append('SparePartId', UUID.SparePartId)
-        console.log("FormData",FormData)
+        console.log("FormData", FormData)
 
         if ((files && files.length > 0) || (pdf && pdf.size > 0)) {
           let upload = await uploadPDF(formData);
@@ -455,6 +455,7 @@ const AddSpare = () => {
     } catch (err) {
       setIsLoading(false);
       notifyError('Error, While Sending File')
+      setLoading(false)
       console.error('Error', err);
       return err;
     }
